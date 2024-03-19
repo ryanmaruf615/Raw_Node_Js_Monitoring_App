860a5cd83e5dc0f30ca408c413ff7ed8dafe595d
Md Maruf Hossain <37552845+ryanmaruf615@users.noreply.github.com>
2024-03-19 01:39:16 +0100

    Update & Delete Done

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -8,6 +8,7 @@
 //dependencies
 const data = require('../../lib/data');
 const {hash} = require('../../helpers/utilities');
+const {parseJSON} = require('../../helpers/utilities');
 
 
 //module scaffolding
@@ -92,14 +93,145 @@ handler._users.post = (requestProperties, callback) => {
 
 
 handler._users.get= (requestProperties,callback) => {
-    callback(200);
+   //check the phone number is valid
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+    if(phone){
+        //lookup the user
+        data.read('users',phone,(err,u)=>{
+            const user = {...parseJSON(u)};
+
+            if(!err){
+                delete user.password;
+                callback(200,user);
+            }else {
+                callback(404,{
+                    'error':'Requested user not found',
+                });
+            }
+        })
+    }else {
+        callback(404,{
+            'error':'Requested user not found',
+        });
+    }
+
 };
 
-handler._users.put= (callback,requestProperties) => {
+handler._users.put= (requestProperties,callback) => {
+    //checking the phone validity
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+
+
+    const firstName =
+        typeof requestProperties.body.firstName === 'string' &&
+        requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : false;
+
+    const lastName =
+        typeof requestProperties.body.lastName === 'string' &&
+        requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : false;
+
+
+    const password =
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : false;
+
+    if(phone){
+        if(firstName || lastName || password ){
+
+            //lookup the user
+            data.read('users',phone,(err,uData)=>{
+                const userData = {...parseJSON(uData)};
+                if(!err && userData){
+                    if(firstName){
+                        userData.firstName = firstName;
+                    }if(lastName){
+                        userData.lastName = lastName;
+                    }if(password){
+                        userData.password = hash(password);
+                    }
+
+                    //store to database
+                    data.update('users',phone,userData,(err)=>{
+                        if (!err){
+                            callback(200,{
+                                'message':'User Update successfully'
+                            })
+                        }else {
+                            callback(500,{
+                                'error':'There was a problem in server side'
+                            });
+                        }
+
+                    })
+                }else {
+                    callback(400,{
+                        'error':'You have problem in your request'
+                    });
+                }
+            })
+        }else {
+            callback(400,{
+                'error':'You have problem in your request'
+            });
+        }
+    }else {
+        callback(400,{
+            'error':'Invalid Pone number'
+        });
+    }
+
 
 };
 
-handler._users.delete= (callback,requestProperties) => {
+handler._users.delete= (requestProperties,callback) => {
+//check the phone number is valid
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+    if(phone){
+        //look up the user
+        data.read('users',phone,(err,u)=>{
+            const userData = {...parseJSON(u)};
 
+            if(!err && userData){
+               data.delete('users',phone,(err)=>{
+                   if (!err){
+                       callback(200,{
+                           'message':'User successfully Deleted'
+                       });
+                   }else {
+                       callback(500,{
+                           'error':'There is a server side error',
+                       });
+                   }
+               });
+
+            }else {
+                callback(500,{
+                    'error':'There is a server side error',
+                });
+            }
+        })
+    }else {
+        callback(400,{
+            'error':'There is a problem in your request'
+        })
+    }
 };
 module.exports = handler;
